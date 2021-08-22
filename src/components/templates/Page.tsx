33c2906b781e9f199889de8c72d7f63aeb81a865
Hayden Isler <me@haydenisler.com>
2021-08-21 23:54:51 -0500

    added button + action to fc page

--- src/components/templates/Page.tsx
+++ src/components/templates/Page.tsx
@@ -8,13 +8,18 @@ type PageProps = {
     children?: ReactNode;
 };
 
-export const Page: FC<PageProps> = ({ title, children }: PageProps) => {
+export const Page: FC<PageProps> = ({
+    title,
+    headerAction,
+    children,
+}: PageProps) => {
     return (
         <div className="flex bg-secondary-700 min-h-screen">
             <Sidebar />
             <main className="min-h-screen flex-grow">
-                <header className="px-8 py-6 border-b border-secondary-500">
+                <header className="px-8 py-[1.10rem] border-b border-secondary-500 flex justify-between items-center">
                     <h1 className="text-xl text-white font-medium">{title}</h1>
+                    <div>{headerAction}</div>
                 </header>
                 <div className="px-8 py-6">{children}</div>
             </main>
