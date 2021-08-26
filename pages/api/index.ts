87872b9a8fe12bb8ea751ab773fd4d38fea446ed
Hayden Isler <me@haydenisler.com>
2021-08-26 15:05:27 -0500

    friendly relation names + logging off
    Co-authored-by: Chamath Dharmasiri  <chamathsd@users.noreply.github.com>

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -3,8 +3,10 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import { prisma } from '../../prisma/db';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-    const result = await prisma.company_craft_draft.findMany();
-    console.log(result);
-
-    res.status(200).json({ name: 'John Doe' });
+    const result = await prisma.company_craft_draft.findMany({
+        where: {
+            id: 0,
+        },
+    });
+    res.status(200).json(result);
 };
